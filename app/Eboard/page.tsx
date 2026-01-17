65a00ba69c20b128eb744c80c1e32fda5c3e9bde
vp690 <vp690@scarletmail.rutgers.edu>
2026-01-16 23:42:09 -0500

    Board Page template made

--- app/Eboard/page.tsx
+++ app/Eboard/page.tsx
@@ -1,15 +1,105 @@
 import Card from "@/app/components/Card";
+import PageHeader from "../components/PageHeader";
 const Eboard = () => {
   return (
-    <Card
-      name="Hasan Ali"
-      position="Education Director"
-      major="CS & Physics"
-      description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
-      description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
-      imageSrc="icons/funny_anime_cs.png"
-      isGod={false}
-    />
+    <div className="eboardPage">
+      <PageHeader>
+        <h1 className="text-[clamp(3rem,8vw,6rem)] font-extrabold tracking-wide text-white">
+          OUR BOARD
+        </h1>
+        <div className="flex flex-wrap justify-evenly">
+          <Card
+            name="Hasan Ali"
+            position="Education Director"
+            major="CS & Physics"
+            description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+            description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+            imageSrc="icons/funny_anime_cs.png"
+            isGod={true}
+          />
+          <Card
+            name="Hasan Ali"
+            position="Education Director"
+            major="CS & Physics"
+            description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+            description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+            imageSrc="icons/funny_anime_cs.png"
+            isGod={true}
+          />
+          <Card
+            name="Hasan Ali"
+            position="Education Director"
+            major="CS & Physics"
+            description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+            description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+            imageSrc="icons/funny_anime_cs.png"
+            isGod={true}
+          />
+        </div>
+      </PageHeader>
+
+      <div className="grid grid-cols-3 gap-6 p-10">
+        <Card
+          name="Hasan Ali"
+          position="Education Director"
+          major="CS & Physics"
+          description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+          description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+          imageSrc="icons/funny_anime_cs.png"
+          isGod={false}
+        />
+
+        <Card
+          name="Hasan Ali"
+          position="Education Director"
+          major="CS & Physics"
+          description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+          description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+          imageSrc="icons/funny_anime_cs.png"
+          isGod={false}
+        />
+
+        <Card
+          name="Hasan Ali"
+          position="Education Director"
+          major="CS & Physics"
+          description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+          description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+          imageSrc="icons/funny_anime_cs.png"
+          isGod={false}
+        />
+
+        <Card
+          name="Hasan Ali"
+          position="Education Director"
+          major="CS & Physics"
+          description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+          description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+          imageSrc="icons/funny_anime_cs.png"
+          isGod={false}
+        />
+
+        <Card
+          name="Hasan Ali"
+          position="Education Director"
+          major="CS & Physics"
+          description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+          description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+          imageSrc="icons/funny_anime_cs.png"
+          isGod={false}
+        />
+
+        <Card
+          name="Hasan Ali"
+          position="Education Director"
+          major="CS & Physics"
+          description1="Hasan Ali is a junior double-majoring in Computer Science and Physics and currently serves as the Education Director of USACS. Passionate about building community and sharing knowledge, Hasan loves connecting with peers and creating opportunities for growth within the organization."
+          description2="Outside of academics, he enjoys playing the saxophone, hitting the tennis courts, reading, and exploring his love for cars. His favorite USACS memory is attending his very first Town Hall, and he’s excited to pursue a career in software engineering."
+          imageSrc="icons/funny_anime_cs.png"
+          isGod={false}
+        />
+      </div>
+    </div>
   );
 };
 
